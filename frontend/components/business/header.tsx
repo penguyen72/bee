fa23f5686a6597ffc18c5b83de8988b0f78e9b32
Peyton Nguyen <peytonn72@gmail.com>
2024-09-28 19:17:06 -0700

    Enhancement: Remove Conditional Header Button

--- frontend/components/business/header.tsx
+++ frontend/components/business/header.tsx
@@ -2,43 +2,36 @@
 
 import { Button } from '@/components/ui/button';
 import { History, Home, Settings, Users } from 'lucide-react';
-import { usePathname, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export function Header() {
-  const pathname = usePathname();
   const router = useRouter();
 
   return (
     <div className="flex items-center gap-4 justify-between w-full">
       <h2 className="text-xl font-semibold">Sun Nails & Spa</h2>
       <div className="flex items-center">
-        {!pathname.includes('/overview') ? (
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={() => router.push('/overview')}
-          >
-            <Home />
-          </Button>
-        ) : null}
-        {!pathname.includes('/transactions') ? (
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={() => router.push('/transactions')}
-          >
-            <History />
-          </Button>
-        ) : null}
-        {!pathname.includes('/members') ? (
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={() => router.push('/members')}
-          >
-            <Users />
-          </Button>
-        ) : null}
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => router.push('/overview')}
+        >
+          <Home />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => router.push('/transactions')}
+        >
+          <History />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => router.push('/members')}
+        >
+          <Users />
+        </Button>
         <Button
           variant="ghost"
           size="icon"
