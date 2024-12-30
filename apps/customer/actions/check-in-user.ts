5771ce26f61b1fa50eee16e80fb5207acd9b5b28
Peyton Nguyen <peytonn72@gmail.com>
2024-12-30 13:00:26 -0500

    Fix: Resolve Customer Unique Id Constraint on Organization and Phone Number (#35)

--- apps/customer/actions/check-in-user.ts
+++ apps/customer/actions/check-in-user.ts
@@ -1,101 +1,96 @@
 "use server"
 
-import { auth } from "@/auth"
+import { ProjectError } from "@/lib/errors"
 import { prisma } from "@/lib/prisma"
 import { formatPhoneNumber } from "@/lib/utils"
 import { SignInSchema } from "@/schemas"
 import { revalidatePath } from "next/cache"
 import { isMobilePhone } from "validator"
 import { z } from "zod"
+import { getOrganization } from "./get-organization"
+
+const validInputs = (values: z.infer<typeof SignInSchema>) => {
+  const { firstName, phoneNumber } = values
+
+  if (!firstName || !phoneNumber)
+    throw new ProjectError({
+      name: "INTERNAL_SERVER_ERROR",
+      message: "All Fields Required!"
+    })
+
+  if (!isMobilePhone(formatPhoneNumber(phoneNumber), "en-US"))
+    throw new ProjectError({
+      name: "INTERNAL_SERVER_ERROR",
+      message: "Invalid Phone Number!"
+    })
+
+  return { firstName, phoneNumber }
+}
 
 export const checkInUser = async (values: z.infer<typeof SignInSchema>) => {
-  try {
-    const session = await auth()
+  const organization = await getOrganization()
 
-    if (!session) return { error: "Unauthorized User!" }
+  const { firstName, phoneNumber } = validInputs(values)
 
-    const email = session.user?.email
-
-    if (!email) return { error: "Invalid Email!" }
-
-    const organization = await prisma.organizations.findUnique({
-      select: {
-        id: true
-      },
-      where: {
-        emailAddress: email
-      }
-    })
-
-    if (!organization) return { error: "Invalid Organization!" }
-
-    const { firstName, phoneNumber } = values
-    if (!firstName || !phoneNumber) {
-      return { error: "All Fields Required!" }
-    }
-
-    if (!isMobilePhone(formatPhoneNumber(phoneNumber), "en-US")) {
-      return { error: "Invalid Phone Number!" }
-    }
-
-    const existingUser = await prisma.customer.findUnique({
-      where: {
-        firstName,
+  const existingUser = await prisma.customer.findUnique({
+    where: {
+      customer_identifier: {
         phoneNumber,
         organizationId: organization.id
+      },
+      firstName
+    }
+  })
+
+  if (!existingUser)
+    throw new ProjectError({
+      name: "INTERNAL_SERVER_ERROR",
+      message: "User Not Found!"
+    })
+
+  const user = await prisma.$transaction(async (tx) => {
+    const latestTransaction = await tx.transactions.findFirst({
+      where: {
+        customerId: existingUser.id,
+        organizationId: organization.id
+      },
+      orderBy: {
+        updatedAt: "desc"
       }
     })
 
-    if (!existingUser) {
-      return { error: "User Not Found!" }
+    if (latestTransaction && !latestTransaction.checkOutTime) {
+      throw new ProjectError({
+        name: "INTERNAL_SERVER_ERROR",
+        message: "User Already Checked In!"
+      })
     }
 
-    const user = await prisma.$transaction(async (tx) => {
-      const latestTransaction = await tx.transactions.findFirst({
-        where: {
-          customerId: existingUser.id,
-          organizationId: organization.id
-        },
-        orderBy: {
-          updatedAt: "desc"
-        }
-      })
-
-      if (latestTransaction && !latestTransaction.checkOutTime) {
-        throw new Error("User Already Checked In!")
-      }
-
-      const customer = await tx.customer.update({
-        where: {
-          firstName: existingUser.firstName,
+    const customer = await tx.customer.update({
+      where: {
+        customer_identifier: {
           phoneNumber: existingUser.phoneNumber,
           organizationId: organization.id
         },
-        data: {
-          visitCount: existingUser.visitCount + 1
-        }
-      })
-
-      await tx.transactions.create({
-        data: {
-          organizationId: organization.id,
-          customerId: customer.id,
-          checkInTime: new Date(),
-          checkOutTime: null,
-          currentPoints: customer.currentPoints
-        }
-      })
-
-      return customer
+        firstName: existingUser.firstName
+      },
+      data: {
+        visitCount: existingUser.visitCount + 1
+      }
     })
 
-    revalidatePath("/", "layout")
-    return { success: "User Checked In", userId: user.id }
-  } catch (error: any) {
-    if (error.message === "User Already Checked In!") {
-      return { error: error.message }
-    }
+    await tx.transactions.create({
+      data: {
+        organizationId: organization.id,
+        customerId: customer.id,
+        checkInTime: new Date(),
+        checkOutTime: null,
+        currentPoints: customer.currentPoints
+      }
+    })
+    return customer
+  })
 
-    return { error: "Internal Server Error!" }
-  }
+  revalidatePath("/", "layout")
+  return user.id
 }
